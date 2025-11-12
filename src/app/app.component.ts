a569f0155c7de37c5ce57eb2f39fcad34463ea2b
𝑱𝒂𝒏 𝑸𝒖𝒆𝒓𝒂𝒍𝒕 <55952925+jaanque@users.noreply.github.com>
2025-11-12 16:11:13 +0100

    hi

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Pokemon } from './interfaces/pokemon.interface';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+
+  pokemons: Pokemon[] = [];
+
+  // Inicialitzem la llista de Pokemons
+  constructor() {
+    this.pokemons = [
+      {id:0, name:"bulbasaur",url:"https://pokeapi.co/api/v2/pokemon/1/",liked: false},
+      {id:1, name:"ivysaur",url:"https://pokeapi.co/api/v2/pokemon/2/",liked: false},
+      {id:2, name:"venusaur",url:"https://pokeapi.co/api/v2/pokemon/3/",liked: false},
+      {id:3, name:"charmander",url:"https://pokeapi.co/api/v2/pokemon/4/",liked: false},
+      {id:4, name:"charmeleon",url:"https://pokeapi.co/api/v2/pokemon/5/",liked: false},
+      {id:5, name:"charizard",url:"https://pokeapi.co/api/v2/pokemon/6/",liked: false},
+      {id:6, name:"squirtle",url:"https://pokeapi.co/api/v2/pokemon/7/",liked: false},
+      {id:7, name:"wartortle",url:"https://pokeapi.co/api/v2/pokemon/8/",liked: false},
+      {id:8, name:"blastoise",url:"https://pokeapi.co/api/v2/pokemon/9/",liked: false},
+      {id:9, name:"caterpie",url:"https://pokeapi.co/api/v2/pokemon/10/",liked: false},
+      {id:10, name:"metapod",url:"https://pokeapi.co/api/v2/pokemon/11/",liked: false},
+      {id:11, name:"butterfree",url:"https://pokeapi.co/api/v2/pokemon/12/",liked: false},
+      {id:12, name:"weedle",url:"https://pokeapi.co/api/v2/pokemon/13/",liked: false},
+      {id:13, name:"kakuna",url:"https://pokeapi.co/api/v2/pokemon/14/",liked: false},
+      {id:14, name:"beedrill",url:"https://pokeapi.co/api/v2/pokemon/15/",liked: false},
+      {id:15, name:"pidgey",url:"https://pokeapi.co/api/v2/pokemon/16/",liked: false},
+      {id:16, name:"pidgeotto",url:"https://pokeapi.co/api/v2/pokemon/17/",liked: false},
+      {id:17, name:"pidgeot",url:"https://pokeapi.co/api/v2/pokemon/18/",liked: false},
+      {id:18, name:"rattata",url:"https://pokeapi.co/api/v2/pokemon/19/",liked: false},
+      {id:19, name:"raticate",url:"https://pokeapi.co/api/v2/pokemon/20/",liked: false}
+    ];  
+
+    // Actualitzem les URLs per obtenir les imatges dels sprites
+    for (let i = 0; i < 20; i++) {
+      this.pokemons[i].url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`;
+    }    
+  }
+
+  // Mètode que rep el Pokemon modificat des del component fill
+  // Busca el Pokemon per ID i actualitza els seus valors
+  updatePokemon(event: Pokemon) {
+    // Troba l'índex del Pokemon a l'array utilitzant el seu ID
+    let idx = this.pokemons.findIndex((value: Pokemon) => value.id === event.id);
+    // Actualitza el Pokemon a l'array amb les noves dades
+    this.pokemons[idx] = event;
+  }
+}
